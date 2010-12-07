27c3d98f8fc3841288b24c6228d68f4fc6dc1b59
Piotr Stefaniak <pstefaniak@atlassian.com>
2010-12-07 14:43:25 +0100

    initial plugin skeleton, from SDK 3.2.3

---
+++ src/test/java/com/atlassian/bamboo/plugins/ExampleServletTest.java
@@ -0,0 +1,15 @@
+package com.atlassian.bamboo.plugins;
+
+import org.junit.Test;
+
+/**
+ * Testing {@link com.atlassian.bamboo.plugins.ExampleServlet}
+ */
+public class ExampleServletTest
+{
+    @Test
+    public void basic()
+    {
+        // add test here...
+    }
+}
