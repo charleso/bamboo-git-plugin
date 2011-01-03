8d5f99595ddfa38b49078bc5a52636e115598b1f
Piotr Stefaniak <pstefaniak@atlassian.com>
2011-01-03 12:24:41 +0100

    upgrade to jgit 0.10.1 (+bugfix)

--- src/main/java/com/atlassian/bamboo/plugins/git/GitRepository.java
+++ src/main/java/com/atlassian/bamboo/plugins/git/GitRepository.java
@@ -28,6 +28,7 @@ import org.jetbrains.annotations.Nullable;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.Serializable;
 import java.util.Collections;
 import java.util.Date;
 import java.util.List;
@@ -56,7 +57,7 @@ public class GitRepository extends AbstractRepository implements MavenPomAccesso
 
     private static final Logger log = Logger.getLogger(GitRepository.class);
 
-    static class GitRepositoryAccessData
+    static class GitRepositoryAccessData implements Serializable
     {
         String repositoryUrl;
         String branch;
