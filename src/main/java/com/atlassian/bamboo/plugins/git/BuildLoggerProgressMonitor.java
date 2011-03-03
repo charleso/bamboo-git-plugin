6dd4a0487a9e760118e1f878baa4cba8115346e3
Slawomir Ginter <sginter@atlassian.com>
2011-03-03 18:12:54 +0100

    BAM-8027: log progress from long running jGit operations every 10 seconds

--- src/main/java/com/atlassian/bamboo/plugins/git/BuildLoggerProgressMonitor.java
+++ src/main/java/com/atlassian/bamboo/plugins/git/BuildLoggerProgressMonitor.java
@@ -3,9 +3,16 @@ package com.atlassian.bamboo.plugins.git;
 import com.atlassian.bamboo.build.logger.BuildLogger;
 import org.eclipse.jgit.lib.ProgressMonitor;
 
+import java.text.MessageFormat;
+
 public class BuildLoggerProgressMonitor implements ProgressMonitor
 {
+    private static final long MESSAGE_INTERVAL = 10000;
+
     private final BuildLogger buildLogger;
+    private long lastMessageTS;
+    private int lastTotalWork;
+    private int lastWork;
 
     public BuildLoggerProgressMonitor(BuildLogger buildLogger)
     {
@@ -18,15 +25,32 @@ public class BuildLoggerProgressMonitor implements ProgressMonitor
 
     public void beginTask(String title, int totalWork)
     {
-        buildLogger.addBuildLogEntry("Git: " + title + " (" + totalWork + ")");
+        lastTotalWork = totalWork;
+        lastWork = 0;
+        String message = MessageFormat.format("Git: {0}{1,choice,0#|1# ({1})}", title, totalWork);
+
+        buildLogger.addBuildLogEntry(message);
+        lastMessageTS = System.currentTimeMillis();
     }
 
     public void update(int completed)
     {
+        lastWork += completed;
+        long now = System.currentTimeMillis();
+        if (now > lastMessageTS + MESSAGE_INTERVAL)
+        {
+            String message = lastTotalWork > 0 ?
+                    MessageFormat.format("Git: {0,number,percent} ({1}/{2})", (double)lastWork/lastTotalWork, lastWork, lastTotalWork) :
+                    MessageFormat.format("Git: ({0})", lastWork);
+
+            buildLogger.addBuildLogEntry(message);
+            lastMessageTS = now;
+        }
     }
 
     public void endTask()
     {
+        lastTotalWork = 0;
     }
 
     public boolean isCancelled()
