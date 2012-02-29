97b580c519ab964947e43524dde88463939c2342
Brydie McCoy <bmccoy@atlassian.com>
2012-02-29 13:48:59 +1100

    Fixing test

--- src/test/java/com/atlassian/bamboo/plugins/git/DetectingBranchesTest.java
+++ src/test/java/com/atlassian/bamboo/plugins/git/DetectingBranchesTest.java
@@ -1,11 +1,14 @@
 package com.atlassian.bamboo.plugins.git;
 
+import com.atlassian.bamboo.plan.branch.VcsBranch;
 import com.atlassian.bamboo.plan.branch.VcsBranchImpl;
 import com.atlassian.testtools.ZipResourceDirectory;
-import com.google.common.collect.Sets;
+import com.google.common.collect.Lists;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.io.File;
+import java.util.List;
 
 import static org.testng.Assert.assertEquals;
 
@@ -20,11 +23,19 @@ public class DetectingBranchesTest extends GitAbstractTest
         GitRepository gitRepository = createGitRepository();
         setRepositoryProperties(gitRepository, testRepository);
 
-        assertEquals(gitRepository.getOpenBranches(), Sets.newHashSet(
+        List<VcsBranch> expectedBranches = Lists.<VcsBranch>newArrayList(
                 new VcsBranchImpl("Branch%with&stupid_chars"),
                 new VcsBranchImpl("a_branch"),
                 new VcsBranchImpl("master")
-        ));
+        );
+
+        List<VcsBranch> openBranches = gitRepository.getOpenBranches();
+
+        Assert.assertEquals(openBranches.size(), expectedBranches.size());
+        for (VcsBranch expected : expectedBranches)
+        {
+            Assert.assertTrue(openBranches.contains(expected));
+        }
     }
 
 }
